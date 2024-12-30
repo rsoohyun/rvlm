a908544774f01b63fed02db66b32072e78e91e9b
rsoohyun <a01026941165@gmail.com>
2024-12-30 23:07:18 +0900

    add celeba

--- data/__init__.py
+++ data/__init__.py
@@ -1,15 +1,16 @@
-from .dataset import WaterBirdsDataset
+from .dataset import WaterBirdsDataset, CelebA
 from .transforms import transforms_preprcs
 
 
 def load_dataset(data_dir, dataset_name, split, transform=None, prompt_id=0):
     if transform is None:
-        transform = transforms_preprcs["waterbird"][split]
+        transform = transforms_preprcs[dataset_name][split]
     
     if dataset_name == "waterbird":
         dataset = WaterBirdsDataset(root=f"{data_dir}/{dataset_name}", split=split, transform=transform, prompt_id=prompt_id)    # 4795
+    elif dataset_name == "celeba":
+        dataset = CelebA(root=f"{data_dir}/{dataset_name}", split=split, transform=transform, prompt_id=prompt_id)
     else:
         raise NotImplementedError(f'{dataset_name} is not supported.')
     
     return dataset
-        
